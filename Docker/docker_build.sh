7266f14beecf3b35cbdd98beb28620f66e53e6cd
Collin L <lkangn.collin@gmail.com>
2024-04-02 14:28:07 +0800

    feat(docker): update

--- Docker/docker_build.sh
+++ Docker/docker_build.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
-if [ -z "${DOCKER_IAMGE}" ] || [ -z "$(docker images -q "${DOCKER_IAMGE}" 2> /dev/null)" ]; then
-	printf "ERROR: docker image not found, DOCKER_IAMGE=%s\n" "${DOCKER_IAMGE}"
-	exit 1
-fi
-
-DOCKERFILE_BUILD_ENABLE=true
-DOCKERFILE="./Dockerfile"
+# NOTE: Please set the follow variables (or save into .envrc) for using this script:
+# - DOCKER_IAMGE		the name of the docker image, which will be used
+# - DOCKERFILE			[optional] the path of dockerfile, default is './Dockerfile'
+# - PROJECT_DIR_PATH		[optional] project root path, default is current script path
+# - DOCKER_BUILD_CMD		[optional] command which use to build image from dockerfile
+#
+# Examples 1:
+# 	env DOCKER_IAMGE=ugw_build ./docker_build.sh make
+#
+# Examples 2:
+# 	echo 'export DOCKER_IAMGE=ugw_build' > .envrc
+# 	./docker_build.sh make
+#
 
 run() {
 	local verbose=1
@@ -19,10 +25,25 @@ run() {
 	"$@" || exit $?
 }
 
+# NOTE: try to load variables from .envrc
+if ! which direnv > /dev/null 2>&1 && [ -e .envrc ]; then
+	run source .envrc
+fi
+
+if [ -z "${DOCKER_IAMGE}" ] || [ -z "$(docker images -q "${DOCKER_IAMGE}" 2> /dev/null)" ]; then
+	printf "ERROR: docker image not found, DOCKER_IAMGE=%s\n" "${DOCKER_IAMGE}"
+	exit 1
+fi
+
+DOCKERFILE_BUILD_ENABLE=true
+DOCKERFILE=${DOCKERFILE:-./Dockerfile}
+DOCKER_BUILD_CMD=${DOCKER_BUILD_CMD:-docker buildx build -t "$DOCKER_IAMGE" -f "${DOCKERFILE}" .}
+
+# don't expand symlinks
 SCRIPT_DIR_PATH=$(dirname "$(realpath -s "$0")")
 
-# NOTE: Assume the projects top level path is same as this script absolute path
-PROJECT_DIR_PATH=${SCRIPT_DIR_PATH}
+# if $PROJECT_DIR_PATH not set, Assume the projects top level path is same as this script absolute path
+PROJECT_DIR_PATH=${PROJECT_DIR_PATH:-${SCRIPT_DIR_PATH}}
 PROJECT_DIR_NAME=$(basename "${SCRIPT_DIR_PATH}")
 RELATIVE_POS=$(realpath -m --relative-to="${PROJECT_DIR_PATH}" "${PWD}")
 CONTAINER_NAME="${PROJECT_DIR_NAME}_$(date +%Y%m%d_%H_%M).${RANDOM}"
@@ -56,10 +77,11 @@ if ${DOCKERFILE_BUILD_ENABLE} && [ -z "$(docker images -q "${DOCKER_IAMGE}" 2> /
 	fi
 
 	echo "Try to build docker image by Dockerfile ..."
-	run docker buildx build -t "$DOCKER_IAMGE" -f "${DOCKERFILE}" .
+	run "${DOCKER_BUILD_CMD}"
 fi
 
-# NOTE: The hook script before running, which use for some projects need to do something to adapt with docker
+# NOTE: The hook script before running docker, which is useful for many projects need to do something to adapt with docker
+# 1. set $OTHER_OPTS array in hook script can import more docker option for project
 if [ -x "${PROJECT_DIR_PATH}/hook_docker_build_pre_run.sh" ]; then
 	run source "${PROJECT_DIR_PATH}/hook_docker_build_pre_run.sh"
 fi
@@ -70,4 +92,8 @@ DOCKER_OPTS=(
 	"${OTHER_OPTS[@]}" 
 	)
 
-run docker run --rm -it "${DOCKER_OPTS[@]}" "${DOCKER_IAMGE}" /usr/bin/bash -c "$*"
+if [ "$#" = "0" ];then
+	run docker run --rm -it "${DOCKER_OPTS[@]}" "${DOCKER_IAMGE}" /usr/bin/bash
+else
+	run docker run --rm -it "${DOCKER_OPTS[@]}" "${DOCKER_IAMGE}" /usr/bin/bash -c "$*"
+fi
