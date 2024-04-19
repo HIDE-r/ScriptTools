add39d28edb413be9c39dbfef9d8bd41a2bf7544
Collin L <lkangn.collin@gmail.com>
2024-04-19 10:28:18 +0800

    feat(docker): update
    1. use bell to notify, when task is done

--- Docker/docker_build.sh
+++ Docker/docker_build.sh
@@ -38,7 +38,8 @@ fi
 
 DOCKERFILE_BUILD_ENABLE=true
 DOCKERFILE=${DOCKERFILE:-./Dockerfile}
-DOCKER_BUILD_CMD=${DOCKER_BUILD_CMD:-docker buildx build -t "$DOCKER_IAMGE" -f "${DOCKERFILE}" .}
+# DOCKER_BUILD_CMD=${DOCKER_BUILD_CMD:-docker buildx build -t "$DOCKER_IAMGE" -f "${DOCKERFILE}" .}
+DOCKER_BUILD_CMD=${DOCKER_BUILD_CMD:-docker build -t "${DOCKER_IAMGE}" -f "${DOCKERFILE}" .}
 
 # don't expand symlinks
 SCRIPT_DIR_PATH=$(dirname "$(realpath -s "$0")")
@@ -46,14 +47,14 @@ SCRIPT_DIR_PATH=$(dirname "$(realpath -s "$0")")
 # if $PROJECT_DIR_PATH not set, Assume the projects top level path is same as this script absolute path
 PROJECT_DIR_PATH=${PROJECT_DIR_PATH:-${SCRIPT_DIR_PATH}}
 PROJECT_DIR_NAME=$(basename "${SCRIPT_DIR_PATH}")
-RELATIVE_POS=$(realpath -m --relative-to="${PROJECT_DIR_PATH}" "${PWD}")
+# RELATIVE_POS=$(realpath -m --relative-to="${PROJECT_DIR_PATH}" "${PWD}")
 CONTAINER_NAME="${PROJECT_DIR_NAME}_$(date +%Y%m%d_%H_%M).${RANDOM}"
 
 DOCKER_BASE_OPTS=(
 	--hostname "${DOCKER_IAMGE}"
 	--name "${CONTAINER_NAME}"
 	--user "$(id -u)":"$(id -g)"
-	-w "${PROJECT_DIR_PATH}/${RELATIVE_POS}"
+	-w "${PWD}"
 	-v "${PROJECT_DIR_PATH}":"${PROJECT_DIR_PATH}"
 	-v /etc/passwd:/etc/passwd:ro
 	-v /etc/shadow:/etc/shadow
@@ -83,7 +84,8 @@ fi
 
 # NOTE: The hook script before running docker, which is useful for many projects need to do something to adapt with docker
 # 1. set $OTHER_OPTS array in hook script can import more docker option for project
-if [ -x "${PROJECT_DIR_PATH}/hook_docker_build_pre_run.sh" ]; then
+if [ -e "${PROJECT_DIR_PATH}/hook_docker_build_pre_run.sh" ]; then
+	chmod u+x "${PROJECT_DIR_PATH}/hook_docker_build_pre_run.sh"
 	run source "${PROJECT_DIR_PATH}/hook_docker_build_pre_run.sh"
 fi
 
@@ -98,3 +100,5 @@ if [ "$#" = "0" ];then
 else
 	run docker run --rm -it "${DOCKER_OPTS[@]}" "${DOCKER_IAMGE}" /usr/bin/bash -c "$*"
 fi
+
+command -v "bell" >/dev/null 2>&1 && bell
